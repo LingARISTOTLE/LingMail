52d6f29ce37ec2a9a775364f8d237ff0645e8b16
Ling <2397328561@qq.com>
2023-05-30 13:30:46 +0800

    fix(重命名引发的idea内部bug):

--- lingmall-coupon/src/test/java/com/ling/lingmallcoupon/LingmallCouponApplicationTests.java
+++ lingmall-coupon/src/test/java/com/ling/lingmallcoupon/LingmallCouponApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ling.lingmailcoupon;
+package com.ling.lingmallcoupon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LingmailCouponApplicationTests {
+class LingmallCouponApplicationTests {
 
     @Test
     void contextLoads() {
